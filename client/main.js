122205506547c8d02a007e7bb4a8eaf9ab4167cf
Lawrence Tran <lawrence@tran.com>
2016-04-16 13:59:31 -0500

    Added google maps

--- client/main.js
+++ client/main.js
@@ -3,6 +3,10 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
+Meteor.startup(function() {
+   GoogleMaps.load();
+});
+
 Template.login.events({
   'click #facebook-login' : function(event) {
     Meteor.loginWithFacebook({}, function(err){
@@ -18,6 +22,17 @@ Template.login.events({
         throw new Meteor.Error("Logout failed");
       }
     });
+  }
+});
+
+Template.map.helpers({
+  mapOptions: function() {
+    if (GoogleMaps.loaded()) {
+      return {
+        center: new google.maps.LatLng(-37.8136, 144.9631),
+        zoom:8
+      };
+    }
   }
 });
 
