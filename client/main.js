32e022363f5a660fb24132282240df50f6f38cc6
Lawrence Tran <lawrence@tran.com>
2016-04-16 13:16:46 -0500

    Added Facebook login and deploy settings

--- client/main.js
+++ client/main.js
@@ -3,20 +3,21 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.login.events({
+  'click #facebook-login' : function(event) {
+    Meteor.loginWithFacebook({}, function(err){
+      if(err) {
+        throw new Meteor.Error("Facebook login failed");
+      }
+    });
+  },
+
+  'click #logout' : function(event) {
+    Meteor.logout(function(err){
+      if (err) {
+        throw new Meteor.Error("Logout failed");
+      }
+    });
+  }
 });
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
-
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+  service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+  service: 'facebook',
+  appId: '1737462789828558',
+  secret: '178e2ceec35b5efeace55f303aa57115'
+});
